5849d7093b750afa5a3f55d299a21a4436174d10
wffpy <wffpy@outlook.com>
2024-07-20 14:44:03 +0000

    refine add op

--- lib/gpu/add.cu
+++ lib/gpu/add.cu
@@ -1,14 +1,14 @@
 #include <cuda_runtime.h>
 #include "gpu/gpu.h"
-// namespace gpu_kernel{
+namespace gpu_kernel{
 // CUDA kernel function to add elements of two arrays
-__global__ void add_gpu(float *a, float *b, float *c, int n) {
+__global__ void add(float *a, float *b, float *c, int n) {
     int index = threadIdx.x + blockIdx.x * blockDim.x;
     if (index < n) {
         c[index] = a[index] + b[index];
     }
 }
-// }   // namespace gpu_kernel
+}   // namespace gpu_kernel
 
 
 namespace gpu {
@@ -26,8 +26,7 @@ int add(float *lhs, float *rhs, float *ret, int length) {
 
     int blockSize = 256;
     int numBlocks = (length + blockSize - 1) / blockSize;
-    // gpu_kernel::add<<<numBlocks, blockSize>>>(d_a, d_b, d_c, length);
-    add_gpu<<<numBlocks, blockSize>>>(d_a, d_b, d_c, length);
+    gpu_kernel::add<<<numBlocks, blockSize>>>(d_a, d_b, d_c, length);
 
     // Copy result from device to host
     cudaMemcpy(ret, d_c, size, cudaMemcpyDeviceToHost);
